f2bf82ee73bbe3d6ba5c3d8c59db2b7567fbc683
Grant Galitz <grantgalitz@gmail.com>
2012-07-26 16:22:57 -0400

    Adding some FLASH support
    FLASH cart code addition. We're trying to be generic and not target a
    specific manufacturer here. Double check Atmel compat later on.

--- IodineGBA/cartridge/GameBoyAdvanceFLASHCore.js
+++ IodineGBA/cartridge/GameBoyAdvanceFLASHCore.js
@@ -22,7 +22,9 @@ function GameBoyAdvanceFlash(IOCore, memoryLarge) {
 GameBoyAdvanceFlash.prototype.initialize = function () {
 	this.FLASH = getUint8Array(this.memorySize);
 	this.bankOffset = 0;
-	this.maxBankOffset = this.memorySize >>> 16;
+	this.maxBankOffset = this.memorySize >>> 17;
+	this.writeStep = 0;
+	this.mode = "NONE";
 }
 GameBoyAdvanceFlash.prototype.load = function (existingData) {
 	var sramLength = existingData.length;
@@ -32,8 +34,98 @@ GameBoyAdvanceFlash.prototype.load = function (existingData) {
 	}
 }
 GameBoyAdvanceFlash.prototype.read = function (address) {
-	return this.FLASH[this.bankOffset | (address & 0xFFFF)];
+	switch (address) {
+		case 0:
+			if (this.mode == "ID") {
+				return 0xBF;
+			}
+			
+		case 1:
+			if (this.mode == "ID") {
+				return 0xD4;
+			}
+		default:
+			return this.FLASH[this.bankOffset | (address & 0xFFFF)];
+	}
 }
 GameBoyAdvanceFlash.prototype.write = function (address, data) {
-	//this.FLASH[this.bankOffset | (address & 0xFFFF)] = data;
+	switch (address) {
+		case 0x5555:
+			switch (this.writeStep) {
+				case 0:
+					if (data == 0xAA) {
+						this.writeStep = 1;
+						break;
+					}
+					else if (data != 0xF0) {
+						this.FLASH[this.bankOffset | address] = data;
+					}
+					this.mode = "NONE";
+					break;
+				case 1:
+					this.writeStep = 0;
+					break;
+				case 2:
+					switch (data) {
+						case 0x90:
+							this.mode = "ID";
+							break;
+						case 0x80:
+							this.mode = "ERASE_COMMAND";
+							break;
+						case 0x10:
+							if (this.mode == "ERASE_COMMAND") {
+								this.mode = "NONE";
+								this.eraseChip();
+							}
+							break;
+						case 0xB0:
+							this.mode = "NONE";
+							this.bankOffset = (data & this.maxBankOffset & 0x1) << 16;
+							break;
+						default:
+							this.mode = "NONE";
+					}
+					this.writeStep = 0;
+			}
+			break;
+		case 0x1000:
+		case 0x2000:
+		case 0x3000:
+		case 0x4000:
+		case 0x5000:
+		case 0x6000:
+		case 0x7000:
+		case 0x8000:
+		case 0x9000:
+		case 0xA000:
+		case 0xB000:
+		case 0xC000:
+		case 0xD000:
+		case 0xE000:
+		case 0xF000:
+			if (this.mode == "ERASE_COMMAND" && this.writeStep == 2 && data == 0x30) {
+				this.eraseSector(address & 0xF000);
+			}
+			else {
+				this.FLASH[this.bankOffset | address] = data;
+			}
+			this.mode = "NONE";
+			this.writeStep = 0;
+			break;
+		case 0x2AAA:
+			if (this.writeStep == 1 && data == 0x55) {
+				this.writeStep = 2;
+			}
+			else {
+				this.mode = "NONE";
+				this.writeStep = 0;
+				this.FLASH[this.bankOffset | address] = data;
+			}
+			break;
+		default:
+			this.mode = "NONE";
+			this.writeStep = 0;
+			this.FLASH[this.bankOffset | address] = data;
+	}
 }
