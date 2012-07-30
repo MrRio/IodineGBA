1de773ab6779ca55cbca2e678375fa6e3453e6a1
Grant Galitz <grantgalitz@gmail.com>
2012-07-30 14:28:57 -0400

    More THUMB v1 mapping

--- IodineGBA/CPU/THUMBInstructionSetCore.js
+++ IodineGBA/CPU/THUMBInstructionSetCore.js
@@ -67,75 +67,127 @@ THUMBInstructionSet.prototype.compileInstructionMap = function () {
 	//1E-1F
 	this.generateLowMap2(this.SUBimm3);
 	//20
-	this.generateLowMap2(this.MOVr0);
+	this.generateLowMap3(this.MOVr0);
 	//21
-	this.generateLowMap2(this.MOVr1);
+	this.generateLowMap3(this.MOVr1);
 	//22
-	this.generateLowMap2(this.MOVr2);
+	this.generateLowMap3(this.MOVr2);
 	//23
-	this.generateLowMap2(this.MOVr3);
+	this.generateLowMap3(this.MOVr3);
 	//24
-	this.generateLowMap2(this.MOVr4);
+	this.generateLowMap3(this.MOVr4);
 	//25
-	this.generateLowMap2(this.MOVr5);
+	this.generateLowMap3(this.MOVr5);
 	//26
-	this.generateLowMap2(this.MOVr6);
+	this.generateLowMap3(this.MOVr6);
 	//27
-	this.generateLowMap2(this.MOVr7);
+	this.generateLowMap3(this.MOVr7);
 	//28
-	this.generateLowMap2(this.CMPr0);
+	this.generateLowMap3(this.CMPr0);
 	//29
-	this.generateLowMap2(this.CMPr1);
+	this.generateLowMap3(this.CMPr1);
 	//2A
-	this.generateLowMap2(this.CMPr2);
+	this.generateLowMap3(this.CMPr2);
 	//2B
-	this.generateLowMap2(this.CMPr3);
+	this.generateLowMap3(this.CMPr3);
 	//2C
-	this.generateLowMap2(this.CMPr4);
+	this.generateLowMap3(this.CMPr4);
 	//2D
-	this.generateLowMap2(this.CMPr5);
+	this.generateLowMap3(this.CMPr5);
 	//2E
-	this.generateLowMap2(this.CMPr6);
+	this.generateLowMap3(this.CMPr6);
 	//2F
-	this.generateLowMap2(this.CMPr7);
+	this.generateLowMap3(this.CMPr7);
 	//30
-	this.generateLowMap2(this.ADDr0);
+	this.generateLowMap3(this.ADDr0);
 	//31
-	this.generateLowMap2(this.ADDr1);
+	this.generateLowMap3(this.ADDr1);
 	//32
-	this.generateLowMap2(this.ADDr2);
+	this.generateLowMap3(this.ADDr2);
 	//33
-	this.generateLowMap2(this.ADDr3);
+	this.generateLowMap3(this.ADDr3);
 	//34
-	this.generateLowMap2(this.ADDr4);
+	this.generateLowMap3(this.ADDr4);
 	//35
-	this.generateLowMap2(this.ADDr5);
+	this.generateLowMap3(this.ADDr5);
 	//36
-	this.generateLowMap2(this.ADDr6);
+	this.generateLowMap3(this.ADDr6);
 	//37
-	this.generateLowMap2(this.ADDr7);
+	this.generateLowMap3(this.ADDr7);
 	//38
-	this.generateLowMap2(this.SUBr0);
+	this.generateLowMap3(this.SUBr0);
 	//39
-	this.generateLowMap2(this.SUBr1);
+	this.generateLowMap3(this.SUBr1);
 	//3A
-	this.generateLowMap2(this.SUBr2);
+	this.generateLowMap3(this.SUBr2);
 	//3B
-	this.generateLowMap2(this.SUBr3);
+	this.generateLowMap3(this.SUBr3);
 	//3C
-	this.generateLowMap2(this.SUBr4);
+	this.generateLowMap3(this.SUBr4);
 	//3D
-	this.generateLowMap2(this.SUBr5);
+	this.generateLowMap3(this.SUBr5);
 	//3E
-	this.generateLowMap2(this.SUBr6);
+	this.generateLowMap3(this.SUBr6);
 	//3F
-	this.generateLowMap2(this.SUBr7);
+	this.generateLowMap3(this.SUBr7);
 	//40
-	this.generateLowMap3(this.AND, this.EOR, this.LSL, this.LSR);
+	this.generateLowMap4(this.AND, this.EOR, this.LSL, this.LSR);
 	//41
-	this.generateLowMap3(this.ASR, this.ADD, this.SUB, this.ROR);
+	this.generateLowMap4(this.ASR, this.ADD, this.SUB, this.ROR);
 	//42
-	this.generateLowMap3(this.TST, this.NEG, this.CMP, this.CMN);
+	this.generateLowMap4(this.TST, this.NEG, this.CMP, this.CMN);
 	//43
-	this.generateLowMap3(this.ORR, this.MUL, this.BIC, this.MVN);
+	this.generateLowMap4(this.ORR, this.MUL, this.BIC, this.MVN);
+	//44
+	this.generateLowMap3(this.ADDH);
+	//45
+	this.generateLowMap3(this.CMPH);
+	//46
+	this.generateLowMap3(this.MOVH);
+	//47
+	this.generateLowMap3(this.BX);
+	//48
+	this.generateLowMap3(this.LDRPCr0);
+	//49
+	this.generateLowMap3(this.LDRPCr1);
+	//4A
+	this.generateLowMap3(this.LDRPCr2);
+	//4B
+	this.generateLowMap3(this.LDRPCr3);
+	//4C
+	this.generateLowMap3(this.LDRPCr4);
+	//4D
+	this.generateLowMap3(this.LDRPCr5);
+	//4E
+	this.generateLowMap3(this.LDRPCr6);
+	//4F
+	this.generateLowMap3(this.LDRPCr7);
+	//50-51
+	this.generateLowMap2(this.STRreg);
+	//52-53
+	this.generateLowMap2(this.STRHreg);
+	//54-55
+	this.generateLowMap2(this.STRBreg);
+	//56-57
+	this.generateLowMap2(this.LDRSBreg);
+	//58-59
+	this.generateLowMap2(this.LDRreg);
+	//5A-5B
+	this.generateLowMap2(this.LDRHreg);
+	//5C-5D
+	this.generateLowMap2(this.LDRBreg);
+	//5E-5F
+	this.generateLowMap2(this.LDRSHreg);
+	//60-67
+	this.generateLowMap(this.STRimm5);
+	//68-6F
+	this.generateLowMap(this.LDRimm5);
+	//70-77
+	this.generateLowMap(this.STRBimm5);
+	//78-7F
+	this.generateLowMap(this.LDRBimm5);
+	//80-87
+	this.generateLowMap(this.STRHimm5);
+	//88-8F
+	this.generateLowMap(this.LDRHimm5);
 }
